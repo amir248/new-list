319855aa95b10ecc6de3a619ad7cb68cd72d967f
amir248 <chikchicly@gmail.com>
2022-05-16 15:45:35 +0700

    window.open

--- src/section.js
+++ src/section.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './section.css';
 import OkImg from '../Impish-Indri.jpg';
 
+import From from './cards/card_one.js';
 
  import { Link } from "react-router-dom"
 
@@ -39,15 +40,33 @@ class Section extends Component {
       console.log("of");
       divEl.remove();
     });
-    const txt='<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
-    divEl.innerHTML=txt;
+    var img = new Image();   // Создаёт новый элемент изображения
+img.src = {OkImg}; // Устанавливает путь
+
+    const txt={'tk': '<p style="display:flex; justify-content:center: align-items:center;width:300px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'};
+    divEl.innerHTML=txt.tk+`<img src={OkImg} alt="images"/>`;
+
     // if(this.state.counter===1){
     //     document.querySelector('.tXt').
     //     document.querySelector('.newElement').prepend(document.querySelector('.tXt'));
     //   document.querySelector('.tXt').style.cssText="dipalay;block";
     // }
 
+
+    // const Image = ()=>{
+    //   return(
+    //     <img src={OkImg} alt="images"/>
+    //   )
+    // }
+
   }
+
+  viewData=()=>{
+      console.log("Clixk");
+          window.open(From,'Data','height=450,width=250');
+      }
+
+
   render() {
     return (
     <React.Fragment>
@@ -57,8 +76,7 @@ class Section extends Component {
 <span> oK new Text to section the future </span>
 <p style={{color:"white"}}> GOOD things ARE G<span style={{color:"red"}}>O</span>ING to HAPPEN</p>
 <strong style={{borderRadius:"10px",background:"rgba(0,0,255,0.3)",display:"grid",alignItems:"center",justifyContent:"center"}} onClick={this.OMG}>Open!</strong>
-
-
+ <div> <p onClick={this.viewData} style={{color:"red"}}>View Data</p></div>
     </React.Fragment>
     );
   }
